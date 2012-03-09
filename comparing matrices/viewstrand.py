1886c57491e18f2767eb12b3a1275cee20298c37
Alex Davis <ecolecule@gmail.com>
2012-03-09 09:51:43 -0500

    checked differences between ompla alignments with bbtm and gonnet series matrices

--- comparing matrices/viewstrand.py
+++ comparing matrices/viewstrand.py
@@ -1,6 +1,9 @@
 from __future__ import division
 from __future__ import print_function
 
+import sys
+sys.path.append("/home/davis/work stuff/beta-barrel-oligomerization/modules")
+
 import numpy as np
 import zenergy
 import csv
@@ -65,26 +68,48 @@ class AlignmentOracle(object):
     def get_pdb_seq_record(self):
         return self.data[self.pdb_index]
 
-abridged_dicts = list()
-for filename in (r'C:\cygwin\home\alex\temp\scry with bbtm.aln',
-                 r'C:\cygwin\home\alex\beta barrels\test moments\ScrY with pdb.aln'):
-    alignment = Bio.AlignIO.read(filename, 'clustal')
-    oracle = AlignmentOracle(alignment, pdb_name = 'chaina')
+# Create dictionaries that map names of proteins to other dictionaries
+bbtm_alignments = dict()
+gonnet_alignments = dict()
 
-    abridged = dict()
-    for i in range(len(alignment)):
-        abridged.update({oracle.data[i].id: (''.join(oracle.sequence(i)))})
+for protein in ('OMPLA',):
+    # Create dictionaries that map names of sequences to sequences containing
+    # only the positions that are aligned to non-gap positions in the pdb
+    # sequence
+    abridged_dicts = list()
+    for filename in (protein + ' bbtm.aln',
+                     protein + ' gonnet.aln'):
+        alignment = Bio.AlignIO.read(filename, 'clustal')
+        oracle = AlignmentOracle(alignment, pdb_name = 'chaina')
 
-    abridged_dicts.append(abridged)
+        abridged = dict()
+        for i in range(len(alignment)):
+            abridged.update({oracle.data[i].id: (''.join(oracle.sequence(i)))})
 
-bbtm = abridged_dicts[0]
-gonnet = abridged_dicts[1]
+        abridged_dicts.append(abridged)
 
-strand_limits = (150, 181)
-print('name then bbtm segment then gonnet segment')
-for name in bbtm.keys():
-    print(name)
-    print(bbtm[name][strand_limits[0]: strand_limits[1]])
-    print(gonnet[name][strand_limits[0]:strand_limits[1]])
-    print()
+    bbtm_alignments.update({protein: abridged_dicts[0]})
+    gonnet_alignments.update({protein: abridged_dicts[1]})
 
+with open('identity.csv', 'rb') as f:
+    id_matrix = matrices.retrieve_matrix(csv.reader(f))
+
+def check_range(start, end, protein):
+    # Observe what positions of each protein in the gonnet alignment and the
+    # bbtm alignment correspond to a given range in the pdb sequence.
+    # Positions ocrresponding to gaps in the pdb sequence not shown.
+    print('name then seq distance then bbtm segment then gonnet segment')
+    pdb_seq_name = 'chaina_' + protein.lower()
+    for name in bbtm_alignments[protein].keys():
+        print(name)
+        print(matrices.compare(bbtm_alignments[protein][name],
+                               bbtm_alignments[protein][pdb_seq_name],
+                               id_matrix))
+        print(matrices.compare(gonnet_alignments[protein][name],
+                               gonnet_alignments[protein][pdb_seq_name],
+                               id_matrix))
+        print(bbtm_alignments[protein][name][start: end])
+        print(gonnet_alignments[protein][name][start:end])
+        print()
+    
+check_range(56,68,'OMPLA')
