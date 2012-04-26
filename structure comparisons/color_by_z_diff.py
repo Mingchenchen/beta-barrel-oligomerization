8e790a7000eaa2d734c34ff7bc955a62faec287f
Alex Davis <ecolecule@gmail.com>
2012-04-26 19:28:14 -0400

    continued z_diff coloration coding

--- structure comparisons/color_by_z_diff.py
+++ structure comparisons/color_by_z_diff.py
@@ -1,38 +1,48 @@
-import csv
 from Bio import AlignIO
 
 def z_diff_blist(target, alignment_path, z_diff_path,
                  alignment_format = 'clustal'):
     # Make a list of residue ids
     stored.resis = list()
-    cmd.iterate(target + 'n. CA',
+    cmd.iterate(target + ' & n. CA',
                 'stored.resis.append(resi)')
-    assert len(stored.resis) == len(set(stored.resis)), \ 
+    assert len(stored.resis) == len(set(stored.resis)),\
            'duplicates in resi list'
     
     # Retrieve sequences as MSA object
     with open(alignment_path, 'r') as f:
         alignment = AlignIO.read(f, alignment_format)
+    # Check that first is 1AF6 and the second is 1A0S
+    
 
     # Retrieve z_diffs as list of floats
     with open(z_diff_path, 'r') as f:
-        z_diffs = [float(i) for i in csv.reader(f)]
+        z_diffs = [float(i) for i in f]
    
     blist = list()
     z_differator = iter(z_diffs)
+    resiterator = iter(resis)
 
-    for resi, letterA, letterB \
-        in zip(stored.resis, alignment[0], alignment[1]):
-        if alignment[0] != '-' and alignment[1] != '-':
-            blist.append(resi, z_differator.next())
+    for letterA, letterB in zip(alignment[0], alignment[1]):
+        if letterA != '-' and letterB != '-':
+            blist.append((resiterator.next(), z_differator.next()))
 
     return blist
 
 def color_b(target, blist):
+    cmd.select('on_blist', 'none')
     for resi, b in blist:
         cmd.alter(target + ' & resi ' + resi,
-                  'b = ' + b)
+                  'b = ' + str(b))
+        cmd.select('on_blist', 'on_blist | resi ' + resi)
 
 def color_by_z_diff(target, alignment_path, z_diff_path,
                     alignment_format = 'clustal'):
-    
+    blist = z_diff_blist(target, alignment_path, z_diff_path,
+                         alignment_format = alignment_format)
+    color_b(target, blist)
+    return len(blist)
+
+print(color_by_z_diff('aligned_1AF6',
+                      'cluster73 bbtmall 1a0s 1af6.aln',
+                      'known 1AF6, unknown 1A0S z_diff, msa, bbtmall.csv'))
