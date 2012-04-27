d1365a52d567cefedcb839e04f6ef0c220420abb
Alex Davis <ecolecule@gmail.com>
2012-04-26 22:56:46 -0400

    created z_diff colorized 1a0s for new york symposium poster

--- structure comparisons/color_by_z_diff.py
+++ structure comparisons/color_by_z_diff.py
@@ -13,19 +13,34 @@ def z_diff_blist(target, alignment_path, z_diff_path,
     with open(alignment_path, 'r') as f:
         alignment = AlignIO.read(f, alignment_format)
     # Check that first is 1AF6 and the second is 1A0S
-    
+    maltoporin = str(alignment[0].seq)
+    assert str(filter(lambda x: x!='-', maltoporin))[:5]=='VDFHG',\
+           'first seq not maltoporin'
+    scry = str(alignment[1].seq)
+    assert str(filter(lambda x: x!='-', scry))[:5]=='SGFEF',\
+           'second seq not sucrose porin'
 
     # Retrieve z_diffs as list of floats
     with open(z_diff_path, 'r') as f:
-        z_diffs = [float(i) for i in f]
+        z_diffs = [abs(float(i)) for i in f]
    
     blist = list()
     z_differator = iter(z_diffs)
-    resiterator = iter(resis)
+    resiterator = iter(stored.resis)
 
-    for letterA, letterB in zip(alignment[0], alignment[1]):
-        if letterA != '-' and letterB != '-':
+
+    print(maltoporin)
+
+
+    for malt_resn, scry_resn in zip(maltoporin, scry):
+        if malt_resn != '-' and scry_resn != '-':
             blist.append((resiterator.next(), z_differator.next()))
+        elif malt_resn == '-' and scry_resn != '-':
+            # No maltoporin residue corresponding to this scry position
+            resiterator.next()
+        elif malt_resn != '-' and scry_resn == '-':
+            # A maltoporin residue that doesn't map onto anywhere
+            pass
 
     return blist
 
@@ -43,6 +58,6 @@ def color_by_z_diff(target, alignment_path, z_diff_path,
     color_b(target, blist)
     return len(blist)
 
-print(color_by_z_diff('aligned_1AF6',
+print(color_by_z_diff('aligned_1A0S',
                       'cluster73 bbtmall 1a0s 1af6.aln',
-                      'known 1AF6, unknown 1A0S z_diff, msa, bbtmall.csv'))
+                      'known 1A0S, unknown 1AF6 z_diff, msa, bbtmall.csv'))
