d2733c4343e983b27d04ed2589597eac0e35f8f4
Alex Davis <ecolecule@gmail.com>
2012-06-08 02:56:18 -0400

    reduced ppi and non ppi dataset spreadsheets

--- jim stuff/reduce selections.py
+++ jim stuff/reduce selections.py
@@ -2,7 +2,7 @@ import os
 import re
 import csv
 
-def reduce(source, target):
+def reduce_sheet(source, target):
     '''Daniel's spreadsheets contain the residue names of the protein,
     as well as all the homology models. This makes them large and slow to
     work with.
@@ -15,9 +15,27 @@ def reduce(source, target):
     Function of two filenames: reduce(source, target)'''
     
     with open(source, 'rb') as s, open(target, 'wb') as t:
-        reader = csv.reader(t)
+        reader = csv.reader(s)
         writer = csv.writer(t)
 
         # Find which column contains the residue identifiers:
         title_line = reader.next()
+        resi_col = title_line.index('ResIndex')
+        last_resi = 0
+        for line in reader:
+            resi = int(line[resi_col])
+            if resi < last_resi:
+                print('last resi in {} is {}'.format(line[0], last_resi))
+                break
+            writer.writerow(line)
+            last_resi = resi
+
+for dataset in ('ppi', 'non ppi'):
+    for filename in os.listdir(dataset + ' residues'):
+        match = re.match('(\d...)\.csv', filename)
+        if match is not None:
+            reduce_sheet('{} residues/{}.csv'\
+                         .format(dataset, match.group(1)),
+                         '{} residues reduced/{}.csv'\
+                         .format(dataset, match.group(1)))
 
