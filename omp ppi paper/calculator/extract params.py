7fecc97a81625e16e6c0ef64718a70a66af922ce
Alex Davis <ecolecule@gmail.com>
2012-07-10 17:22:09 -0400

    finished ez-beta asymmetric calculator

--- omp ppi paper/calculator/extract params.py
+++ omp ppi paper/calculator/extract params.py
@@ -1,15 +1,17 @@
 import csv
 
 # The residue names in the order that I want them:
-resns = ['A', 'L', 'I', 'V', 'D', 'E', 'K', 'N', 'P', 'Q',
-         'R', 'H', 'S', 'F', 'W', 'Y', 'G']
+resns = ['A', 'L', 'I', 'V', 'M', 'D', 'E', 'K', 'N', 'P', 'Q',
+         'R', 'H', 'S', 'T', 'F', 'W', 'Y', 'G']
 
 sideways = list()
 
 # Create the first two columns
-column1 = ['e0+', 'zmid+', 'n+', 'e0-', 'zmid-', 'n-']
+column1 = ['', 'curve', 'Paq+', 'e0+', 'zmid+', 'n+',
+           'Paq-', 'e0-', 'zmid-', 'n-']
 sideways.append(column1)
-column2 = ['emin+', 'zmin+', 'sigma-', 'emin-', 'zmin-', 'sigma-']
+column2 = ['', '', 'Paq+', 'emin+', 'zmin+', 'sigma+',
+           'Paq-', 'emin-', 'zmin-', 'sigma-']
 sideways.append(column2)
 
 
@@ -22,15 +24,17 @@ for resn in resns:
 
         # The first two entries in this column are nTot and nRes, which
         # I don't need
-        params = params[3:]
-        # The first and fifth entries are Paq, which I don't need
-        params = params[1:5] + params[6:]
+        params = params[2:]
         
-        # I'll fill in the curve type (second entry in each column) manually
-        column = [resn, ''] + params
+        if resn in ['F', 'W', 'Y', 'G']:
+            curvetype = 'gaussian'
+        else:
+            curvetype = 'sigmoidal'
+
+        column = [resn, curvetype] + params
         sideways.append(column)
 
-with open('asymmetric params, bbtmout.csv', 'wb'):
+with open('asymmetric params, bbtmout.csv', 'wb') as f:
     csv.writer(f).writerows(zip(*sideways))
     
         
